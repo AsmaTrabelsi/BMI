b659fdee531f2e4b01534ad7ed1af3ba4006bf91
unknown <asmatra149@gmail.com>
2021-06-21 22:52:05 +0100

    add missing files
    add missing files

---
+++ app/src/main/java/com/example/bmi/vue/Launcher.java
@@ -0,0 +1,16 @@
+package com.example.bmi.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bmi.R;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
